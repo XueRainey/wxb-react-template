2a25fe97a16f4ca7f46a4d272324ca635eaa94b9
Rainey <515385036@qq.com>
2017-03-10 11:11:08 +0800

    优化webpack.puglin

--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -2,56 +2,39 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const Dashboard = require('webpack-dashboard');
 const DashboardPlugin = require('webpack-dashboard/plugin');
-const dllManifest = require('./dll.manifest.json');
 
 module.exports = function getPlugins(env) {
+    const plugins = [];
+    const pluginModule = [];
     // 定义运行环境全局变量
-    const defineVariable = new webpack.DefinePlugin({
+    plugins.push(new webpack.DefinePlugin({
         // 定义当前运行环境，在运行环境中可以通过process.env.NODE_ENV访问当前环境
         'process.env.NODE_ENV': JSON.stringify(env),
-    });
+    }));
     // css代码编译
     const extractCSS = new ExtractTextPlugin('[name].css');
+    plugins.push(extractCSS);
 
-    const dllPlugin = new webpack.DllReferencePlugin({
-        context: __dirname,
-        manifest: dllManifest
-    });
+    // 使用dll
+    try {
+        plugins.push(new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./dll.manifest.json')
+        }));
+    } catch(e) {
+        console.log('未检测到dll.manifest.json文件，默认打包全部文件');
+    }
 
     if (env === 'develop') {
         // 代码热更新
-        const hotReplace = new webpack.HotModuleReplacementPlugin();
+        plugins.push(new webpack.HotModuleReplacementPlugin());
         // 控制台增强
-        const dashboard = new DashboardPlugin(new Dashboard().setData);
-        return {
-            plugins: [
-                dllPlugin,
-                defineVariable,
-                extractCSS,
-                hotReplace,
-                dashboard
-            ],
-            pluginModule: {
-                extractCSS
-            }
-        };
+        plugins.push(new DashboardPlugin(new Dashboard().setData));
     }
 
-    if (env === 'produce') {
-        return {
-            plugins: [
-                dllPlugin,
-                defineVariable,
-                extractCSS
-            ],
-            pluginModule: {
-                extractCSS
-            }
-        };
-    }
+    // if (env === 'produce') {
 
-    return {
-        plugins: [],
-        pluginModule: {}
-    };
+    // }
+
+    return { plugins, pluginModule: { extractCSS } };
 };
